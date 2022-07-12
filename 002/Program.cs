ef0a2a89e1c338a6bbf91b75b7719c52efcaa070
Roman <igoshinr.estho@gmail.com>
2022-07-13 02:21:12 +0300

    002

---
+++ 002/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+if (a < b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
