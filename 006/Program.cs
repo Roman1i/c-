12e2490a2211cc1afd79eadca561184a707f4f68
Roman <igoshinr.estho@gmail.com>
2022-07-13 02:35:34 +0300

    006

---
+++ 006/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+bool what;
+
+if (n % 2 == 0)
+{
+    what = true;
+    Console.WriteLine(what);
+}
+if (n % 2 != 0)
+{
+    what = false;
+    Console.WriteLine(what);
+}
